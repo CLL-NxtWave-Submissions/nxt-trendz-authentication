8cc510cc193d8240d96ca2cf03d78c150313a095
Surya T N <suryatej.nim@yahoo.com>
2022-10-11 05:42:50 +0000

    Added code to handle routing of different paths in App component, with imports from react-router-dom and other app components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import {Switch, Route} from 'react-router-dom'
+
 import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
 
 import './App.css'
 
-const App = () => <LoginForm />
+const App = () => (
+  <Switch>
+    <Route path="/login" component={LoginForm} />
+    <Route path="/" component={Home} />
+    <Route component={NotFound} />
+  </Switch>
+)
 
 export default App
