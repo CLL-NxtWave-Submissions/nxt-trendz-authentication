9b2881f9566b0058300424deb1f6e123fd97ff65
Surya T N <suryatej.nim@yahoo.com>
2022-10-12 08:27:26 +0000

    Added new home container JSX element around the content container and the main image, within the JSX element tree of Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,29 +4,31 @@ import './index.css'
 const Home = () => (
   <div className="home-bg-container">
     <Header />
-    <div className="home-content-container">
-      <h1 className="home-content-header">Clothes That Get You Noticed</h1>
+    <div className="home-container">
+      <div className="home-content-container">
+        <h1 className="home-content-header">Clothes That Get You Noticed</h1>
+        <img
+          className="home-content-column-image"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+          alt="clothes that get you noticed"
+        />
+        <p className="home-content-description">
+          Fashion is part of the daily air and it does not quite help that it
+          changes all the time. Clothes have always been a marker of the era and
+          we are in a revolution.Your fashion makes you been seen and heard that
+          way you are. So, celebrate the seasons new and exciting fashion in
+          your own way.
+        </p>
+        <button className="home-content-shop-now-button" type="button">
+          Shop Now
+        </button>
+      </div>
       <img
-        className="home-content-column-image"
+        className="home-row-image"
         src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
         alt="clothes that get you noticed"
       />
-      <p className="home-content-description">
-        Fashion is part of the daily air and it does not quite help that it
-        changes all the time. Clothes have always been a marker of the era and
-        we are in a revolution.Your fashion makes you been seen and heard that
-        way you are. So, celebrate the seasons new and exciting fashion in your
-        own way.
-      </p>
-      <button className="home-content-shop-now-button" type="button">
-        Shop Now
-      </button>
     </div>
-    <img
-      className="home-row-image"
-      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
-      alt="clothes that get you noticed"
-    />
   </div>
 )
 
